84ea09904592302a41e6b10104ff99bf59019f69
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-07-24 21:56:10 +0300

    Update websocket.cpp

--- src/network/http/websocket.cpp
+++ src/network/http/websocket.cpp
@@ -321,7 +321,7 @@ namespace fc { namespace http {
                //if( server_pem.size() )
                {
                   _server.set_tls_init_handler( [=]( websocketpp::connection_hdl hdl ) -> context_ptr {
-                        context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::tlsv1);
+                        context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::tlsv12);
                         try {
                            ctx->set_options(boost::asio::ssl::context::default_workarounds |
                            boost::asio::ssl::context::no_sslv2 |
